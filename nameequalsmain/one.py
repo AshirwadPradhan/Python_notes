5470957dda36af7e1538f6b662b458b3cefb5ea1
AshirwadPradhan <ashirwad.pradhan007@gmail.com>
2018-03-13 09:20:11 +0530

    added all the python notes based on sections

---
+++ nameequalsmain/one.py
@@ -0,0 +1,16 @@
+#one.py
+def func():
+	print('func() in one.py')
+
+def function():
+	pass
+
+def function2():
+	pass
+
+
+if __name__ == '__main__':
+	#run the script
+	function2()
+	function()
+	#this basically allowes for code organization
---
+++ unit_test/cap.py
@@ -0,0 +1,7 @@
+def cap_text(text):
+	'''
+	Input: a string
+	Output: capitalize a string
+	''' 
+	#return text.capitalize()    # failed scenario
+	return text.title()			 # success scenario
